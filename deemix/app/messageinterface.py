2953f6dd62c1397fc9daca6325a1d4dcbcfeb03f
segevp <macsegev@gmail.com>
2020-09-11 18:19:08 +0300

    override necessary deemix functions and remove its files from repo

--- deemix/app/messageinterface.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-class MessageInterface:
-    def send(self, message, value=None):
-        """Implement this class to process updates and messages from the core"""
-        pass
--- segevmusic/deezer.py
+++ segevmusic/deezer.py
@@ -1,8 +1,14 @@
 from os.path import realpath, join
 from deemix.app.cli import cli
+from deemix.app.settings import Settings
+from segevmusic import overriders
 
 DEEZER_ISRC_QUERY = r"https://api.deezer.com/2.0/track/isrc:{isrc}"
 ARL = r"5bbd39c9df0b86568f46c9310cb61f4c9c3e3a1cef78b0a5e142066dca8c1ea495edea03cbb1536a5ba1fd2cff9b15fe21114d221140b57e0ab96484d4a1f4d0acbbfe66af7587a8f2af59ebeb5036c7d09bd1d8ad936f4da1b9c1ed6af46e21"
+
+# Override deemix functions
+cli.login = overriders.cli_login
+Settings.__init__ = overriders.settings_init
 
 
 class DeezerFunctions:
