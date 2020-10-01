0fe8ebbccc212c7a5fc1476c78b5c035bf898e83
segevp <macsegev@gmail.com>
2020-10-02 01:56:12 +0300

    wetransfer verbosity improvements

--- segevmusic/wetransfer.py
+++ segevmusic/wetransfer.py
@@ -85,12 +85,12 @@ class WTSession(requests.Session):
         chunk_number = 0
         while True:
             print("\r--> {0:.2f}% uploaded...".format(self.current_chunk * 100 / self.total_chunks),
-                  f"--> Started uploading {file_name}...",
+                  f"Started uploading {file_name}...",
                   sep=' \\ ', end='')
             chunk = f.read(default_chunk_size)
             chunk_size = len(chunk)
             if chunk_size == 0:
-                print(f"\n--> Finished uploading {file_name}.")
+                print(f"\r--> Finished uploading {file_name}.")
                 break
             chunk_number += 1
             self.current_chunk += 1
